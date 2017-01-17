58e23c5a2de5d7e87264b376c4438325864169cc
p01K <pkatsogr@gmail.com>
2017-01-17 13:20:03 +0100

    no exception

--- src/main/scala/simple/Profiling.scala
+++ src/main/scala/simple/Profiling.scala
@@ -28,7 +28,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import scala.io.Source
 import scala.util.Random
-import com.github.nscala_time.time.Imports._
+// import com.github.nscala_time.time.Imports._
 import org.joda.time.{DateTime, DateTimeConstants}
 import org.joda.time.format.DateTimeFormat
 
@@ -47,26 +47,28 @@ case class Profile(val region: Int, val week_id: Int, is_we: Boolean, day_time: 
 object CDR {
   def from_string(row: String, field2col: Map[String,Int]) = {
     val split      = row.split(";")
-    val user_id    = split( field2col("user_id") ).toInt
-    val start_cell = split( field2col("start_cell") ).toInt
-    val end_cell   = split( field2col("end_cell") ).toInt
+    val user_id    = split( field2col("user_id") ).toLong
+    val start_cell = split( field2col("start_cell") )
+    val end_cell   = split( field2col("end_cell") )
     val date       = string_to_date( split( field2col("date") ) )
-    val time       = split( field2col("date") ).toInt
+    val time       = split( field2col("time") ).toInt
 
     new CDR(user_id, start_cell, end_cell, date, time)
   }
 
   def string_to_date(str: String): DateTime = {
     val datePattern = "yyyy-MM-dd"
-    val timePattern = "HHmmss"
-    val datetimeDelim = ":"
+    val timePattern = "HH:mm:ss"
+    val datetimeDelim = " "
+    val datetimePattern = Array(datePattern, timePattern).mkString(datetimeDelim)
     val dateFormat = DateTimeFormat.forPattern(datePattern)
-    dateFormat.parseDateTime(str)
+    val datetimeFormat = DateTimeFormat.forPattern(datetimePattern)
+    datetimeFormat.parseDateTime(str)
   }
 
 }
 
-class CDR(val user_id: Int, start_cell: Int, end_cell: Int, date: DateTime, val time: Int){
+class CDR(val user_id: Long, start_cell: String, end_cell: String, date: DateTime, val time: Int){
 
   def is_we(): Boolean = { Range(0,6).contains( date.dayOfWeek().get()) }
 
@@ -74,9 +76,9 @@ class CDR(val user_id: Int, start_cell: Int, end_cell: Int, date: DateTime, val 
 
   def year(): Int = { date.year().get() }
 
-  def region(cell2region: Map[Int,Int]): Int = { cell2region(start_cell) }
+  def region(cell2region: Map[String,Int]): Int = { cell2region(start_cell) }
 
-  def valid_region(cell2region: Map[Int,Int]): Boolean = { cell2region.contains(start_cell) }
+  def valid_region(cell2region: Map[String,Int]): Boolean = { cell2region.contains(start_cell) }
 
   def day_time(): Int = { time }
 
@@ -114,7 +116,7 @@ object UserProfiling {
   def parseArguments(args: Array[String]) = {
     var master :String  = null
     var spatial:String  = null
-    var field  :String  = null
+    // var field  :String  = null
 
     System.err.println(s"options: ${args.toList.mkString(" ")}")
 
@@ -127,32 +129,35 @@ object UserProfiling {
         case ("--spatial_div") :: value :: tail =>
           spatial = value
           argv   = tail
-        case ("--field") :: value :: tail =>
-          field = value
-          argv   = tail
+        // case ("--field") :: value :: tail =>
+        //   field = value
+        //   argv   = tail
         case Nil =>
         case tail =>
           System.err.println(s"Unrecognized options: ${tail.mkString(" ")}")
           printUsageAndExit()
       }
     }
-    (master,spatial,field)
+    (master,spatial)
   }
 
 
   def main(args: Array[String]) = {
-
-    val (master,spatial_div,field_file) = parseArguments(args)
+    val spatial_div = "/home/forth/asap4all/telecom-analytics/src/python/spatial_regions/aree_roma.csv"
+    val (master,_) = parseArguments(args)
 
     val cell2munic = Source.fromFile(spatial_div).getLines().map( line => {
       val split = line.split(";")
-      (split(0).toInt,split(1).trim.replace("\n","").toInt)
+      (split(0),split(1).trim.replace("\n","").toInt)
     }).toMap
 
-    val mutfield2col = Source.fromFile(field_file).getLines().map( line => {
-      val split = line.split(",")
-      (split(0),split(1).trim.toInt)
-    }).toMap
+   val field2col: Map[String,Int] =
+   Map("user_id" -> 0, "cdr_type" -> 11, "start_cell" -> 9 ,"end_cell"->10 , "date" -> 3 ,"time"-> 4)
+
+    // val mutfield2col = Source.fromFile(field_file).getLines().map( line => {
+    //   val split = line.split(",")
+    //   (split(0),split(1).trim.toInt)
+    // }).toMap
 
     val conf = new SparkConf().setAppName("Benchmark").setMaster(master)
                               .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
@@ -162,23 +167,29 @@ object UserProfiling {
 
     val sc = new SparkContext(conf)
 
-    val files = "lalala"
+    val data = sc.textFile("/dataset_simulated//REPORT_ESTRAZ_TRAFF_COMUNE_ROMA_2016*.csv")
+      .map( line => CDR.from_string(line,field2col) )
+      .filter( cdr => cdr.valid_region(cell2munic) ).cache()
 
-    val data = sc.textFile("files")
-      .map( line => CDR.from_string(line,mutfield2col) )
-      .filter( cdr => cdr.valid_region(cell2munic) )
+    val count = data.count()
+    println(s"Data count == $count")
+    val weeks = Array.tabulate(4)(i => i+4) // hardcoded for now
 
-    val weeks = Array.tabulate(4)(i => i) // hardcoded for now
-
-    val wgroups = weeks.zipWithIndex.filter{ case (w,i) => i%4==0 }
+    val wgroups = weeks.filter{ case w => w%4==0 }.zipWithIndex
 
     wgroups.foreach{ case(w,i) =>
+      println(s"$w $i")
       val slice = weeks.slice(i,i+4)
-      val profiles = data.filter( cdr => slice.contains(cdr.week()) ).map( cdr =>
+      println(s"${slice.mkString(",")}")
+      val profiles = data
+        .filter( cdr => slice.contains(cdr.week()) )
+        .map( cdr =>
         ((cdr.user_id,cdr.region(cell2munic),slice.indexOf(cdr.week()),cdr.is_we(),cdr.day_time()),1)
-      ).reduceByKey{ case(v1,v2) => v1+v2 }
-        // .map()
+      ).collect()
+        // .reduceByKey{ case(v1,v2) => v1+v2 }.collect()
+    //     // .map()
         // .reducrByKey{ case(v1,v2) => v1++v2} //concat
+      profiles.foreach( p => println(p) )
     }
     // val weeks = ....
     // group weeks
